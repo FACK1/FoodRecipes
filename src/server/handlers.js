4ad683efc88ccd4ce113b79b7f8c76313a950be2
safaaamro <safaaamro072@gmail.com>
2018-11-28 09:25:42 +0200

    update handler#5

--- src/server/handlers.js
+++ src/server/handlers.js
@@ -16,6 +16,41 @@ const  homeHandler=(request,response)=>{
   });
 }
 
-// const publicHandler=(request, response)=>{
-//   const filePath=(path.join(__dirname,))
-// }
+const publicHandler = (request, response) => {
+  const extension = request.url.split('.')[1];
+  const contentTypeMapping = {
+    'html':'text/html',
+    'css': 'text/css',
+    'js': 'application/js'
+  };
+  if(!contentTypeMapping[extension]){
+    response.writeHead(404,{'Content-Type': 'text/html'});
+    response.end('<h1> Not Found </h1>')
+  } else {
+  const filePath = path.join(__dirname,'..','public, request.url);
+  fs.readFile(filePath, (error, file)=>{
+    if (error) {
+      errorHandler( request, response);
+    }
+    response.writeHead(200, {'content-type': contentTypeMapping[extension]})
+    response.end(file);
+  });
+  }
+};
+
+const errorHandler = (request, response) => {
+  response.writeHead(404,{'content-type':'text/html'})
+  response.end('<h1> Page Not Found </h1>');
+};
+
+const searchHandler=(request, response)=>{
+  const searchValue = request.url.split('/')[2];
+  if (!searchValue) {
+    errorHandler(request, response);
+  } else {
+    const reslut = searchRecipes(searchValue);
+    var convertData= json.stringify(reslut);
+    response.writeHead (200, {'content-type': 'application/js'});
+    response.end(convertData);
+  }
+};
